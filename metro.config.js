7abcf51ed70af512686007ac279836bf0dc0bd90
Saymon Araújo <saymonbrandon@gmail.com>
2023-03-03 09:28:15 -0300

    chore: add metro handler

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,30 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// packages/mobile/metro.config.js
+const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+
+const symlinkResolver = MetroSymlinksResolver();
+
+const requestResolver = (context, moduleName, platform, realName) => {
+  // patch for unpatched dependencies
+  // e.g. react-native-version-check-expo
+  if (moduleName === "@unimodules/core") {
+    const expoModules = "expo-modules-core";
+    return symlinkResolver(context, expoModules, platform, expoModules);
+  }
+
+  return symlinkResolver(context, moduleName, platform, realName);
+};
+
+module.exports = {
+  ...defaultConfig,
+  ...makeMetroConfig({
+    projectRoot: __dirname,
+    resolver: {
+      resolveRequest: requestResolver,
+    },
+  }),
+};
